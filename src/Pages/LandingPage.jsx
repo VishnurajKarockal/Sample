a4663e30f36429af7e49d3a8be220a1ae518e51b
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-02-20 17:35:00 +0530

    Vishnu_sample

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import { Box, HStack, Link, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+   <Box>
+    <HStack>
+        <VStack>
+            <Text>hdhqijhoi</Text>
+            <Text>shfikwfioio</Text>
+        </VStack>
+        <VStack>
+            <Link to="/videos">Videos</Link>
+        </VStack>
+        
+    </HStack>
+   </Box>
+  )
+}
+
+export default LandingPage
